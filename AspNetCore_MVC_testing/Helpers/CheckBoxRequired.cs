9b10d811db4755bde9b123348bcfebfb2f2fc1d9
victor <myran_virre@hotmail.com>
2024-02-23 17:34:50 +0100

    sparar...

---
+++ AspNetCore_MVC_testing/Helpers/CheckBoxRequired.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCore_MVC_testing.Helpers;
+
+public class CheckBoxRequired : ValidationAttribute
+{
+
+        public override bool IsValid(object? value) => value is bool b && b;
+    
+}
--- AspNetCore_MVC_testing/Models/SignUpModel.cs
+++ AspNetCore_MVC_testing/Models/SignUpModel.cs
@@ -1,39 +1,45 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using AspNetCore_MVC_testing.Helpers;
+using System.ComponentModel.DataAnnotations;
 
 namespace AspNetCore_MVC_testing.Models;
 
 public class SignUpModel
 {
     [Display(Name = "First name", Prompt = "Enter your first name", Order = 0)]
-    [Required(ErrorMessage = "Invalid first name")]
+    [Required(ErrorMessage = "First name is required")]
     public string FirstName { get; set; } = null!;
 
 
     [Display(Name = "Last name", Prompt = "Enter your Last name", Order = 1)]
-    [Required(ErrorMessage = "Invalid first name")]
+    [Required(ErrorMessage = "Last name is required")]
     public string LastName { get; set; } = null!;
 
 
     [Display(Name = "Email adress", Prompt = "Enter email", Order = 2)]
     [DataType(DataType.EmailAddress)]
-    [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email")]
+    [Required(ErrorMessage = "Email is required")]
+    [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Your email is invalid")]
 
     public string Email { get; set; } = null!;
 
 
     [Display(Name = "Password", Prompt = "Enter password", Order = 3)]
     [DataType(DataType.Password)]
-    [RegularExpression(@"^(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$", ErrorMessage = "Invalid password")]
+    [Required(ErrorMessage = "Password is required")]
+    [RegularExpression(@"^(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$", ErrorMessage = "Invalid password, must be strong password")]
     public string Password { get; set; } = null!;
 
 
     [Display(Name = "Confirm Password", Prompt = "Confirm password", Order = 4)]
     [DataType(DataType.Password)]
     [Compare(nameof(Password), ErrorMessage = "Passwords does not match")]
+    [Required(ErrorMessage = "Password must be confirmed")]
     public string ConfirmPassword { get; set; } = null!;
 
 
     [Display(Name = "I agree to the Terms & Conditions", Order = 5)]
-    [Required(ErrorMessage = "Invalid first name")]
+    [Required(ErrorMessage = "You must accept the terms and conditions.")]
+    [CheckBoxRequired(ErrorMessage = "You must accept the terms and conditions.")]
     public bool TermsAndConditions { get; set; } = false;
 }
+
